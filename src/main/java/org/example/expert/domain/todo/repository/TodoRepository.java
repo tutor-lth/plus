00416339e78fdd4a7cca3a64ae8a82647a6cf736
sparta7tutor@gmail.com <sparta7tutor@gmail.com>
2025-12-29 17:55:04 +0900

    feat: 2-8 QueryDSL

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -5,19 +5,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryDslRepository {
 
     @Query("""
-               SELECT t FROM Todo t
-      LEFT JOIN FETCH t.user u
-                WHERE (:weather IS NULL OR t.weather = :weather)
-                  AND (:startAt IS NULL OR t.modifiedAt >= :startAt)
-                  AND (:endAt IS NULL OR t.modifiedAt <= :endAt)
+             SELECT t FROM Todo t
+             LEFT JOIN FETCH t.user u
+             WHERE (:weather IS NULL OR t.weather = :weather)
+             AND (:startAt IS NULL OR t.modifiedAt >= :startAt)
+             AND (:endAt IS NULL OR t.modifiedAt <= :endAt)
              ORDER BY t.modifiedAt DESC
              """)
     Page<Todo> searchTodos(
@@ -26,9 +24,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             LocalDateTime startAt,
             LocalDateTime endAt
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
